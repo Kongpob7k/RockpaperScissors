249cffe42e07da499e29b3acd991607503ee235c
Kongpob <kongpop@gmail.com>
2025-01-04 12:35:20 +0700

    Ugly Ui

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
 const Choice=["rock","paper","scissors"];
+const btn=document.querySelectorAll("button");
 
 let HumanScore=0,ComputerScore=0;
+
 
 function getComputerChoice(){
     return Choice[Math.floor(Math.random()*3)];
 }
 
-function getHumanChoice(){
-    while(true){
-        let Human=prompt("What is your Choice"); 
-        Human.toLowerCase();
-        if(Human!="rock"&&Human!="paper"&&Human!="scissors"){
-            alert("You Entered wrong choice please try again");
-            continue;
-        }
-        return Human;
-    }
-}
-
 function PlayRound(HumanChoice,ComputerChoice){
-    while((HumanScore!=5) || (ComputerScore!=5)){
-        HumanChoice=getHumanChoice();
-        ComputerChoice=getComputerChoice();
-        if(HumanChoice===ComputerChoice)continue;
-        else if(HumanChoice==="rock"){
-            if(ComputerChoice==="paper")ComputerScore++;
-            else if(ComputerChoice==="scissors")HumanScore++;
-        }
-        else if(HumanChoice==="paper"){
-            if(ComputerChoice==="rock")HumanScore++;
-            else if(ComputerChoice==="scissors")ComputerScore++;
-        }
-        else if(HumanChoice==="scissors"){
-            if(ComputerChoice==="rock")ComputerScore++;
-            else if(ComputerChoice==="paper")HumanScore++;
-        }
-        console.log(HumanChoice+" "+ComputerChoice+"\n");
+    if(HumanChoice==="rock"){
+        if(ComputerChoice==="paper")ComputerScore++;
+        else if(ComputerChoice==="scissors")HumanScore++;
     }
-    if(HumanScore===5)console.log("HUMAN WIN");
-    else console.log("COMPUTER WIN");
+    else if(HumanChoice==="paper"){
+        if(ComputerChoice==="rock")HumanScore++;
+        else if(ComputerChoice==="scissors")ComputerScore++;
+    }
+    else if(HumanChoice==="scissors"){
+        if(ComputerChoice==="rock")ComputerScore++;
+        else if(ComputerChoice==="paper")HumanScore++;
+    }
 }
 
-PlayRound(getHumanChoice(),getComputerChoice())
+for(let i=0;i<btn.length;i++){
+    btn[i].addEventListener("click", ()=> {
+        const ComputerChoice=getComputerChoice();
+        PlayRound(Choice[i],ComputerChoice)
+        if(HumanScore===5)alert("PLAYER WIN!");
+        else if(ComputerScore===5)alert("COMPUTER WIN!");
+        const ComputerText=document.querySelector(".Text-Computer");
+        const PlayerText=document.querySelector(".Text-Player");
+        ComputerText.textContent = "Computer Score:"+ComputerScore+"\nComputer Choose"+ComputerChoice;
+        ComputerText.style.whiteSpace = "pre-line";
+        PlayerText.textContent = "Player Score:"+HumanScore+"\nPlayer Choose"+Choice[i];
+        PlayerText.style.whiteSpace = "pre-line";
+        console.log(HumanScore+" "+ComputerScore);
+    });
+}
 
-console.log(getHumanChoice());
+
